480623067c3b6980f5214baee165226c05f5464d
Matheus Gomes <47065962+matheus369k@users.noreply.github.com>
2024-07-04 19:08:43 -0300

    create browser routes of the project

---
+++ src/router/Router.tsx
@@ -0,0 +1,27 @@
+import { createBrowserRouter } from 'react-router-dom';
+
+import { LayoutDefault } from '@/layout';
+import { Checkout } from '@pages/Checkout';
+import { Confirm } from '@pages/Confirm';
+import { Home } from '@pages/Home';
+
+export const router = createBrowserRouter([
+    {
+        path: '/coffee-delivery/',
+        element: <LayoutDefault />,
+        children: [
+            {
+                path: '/coffee-delivery/',
+                element: <Home />,
+            },
+            {
+                path: '/coffee-delivery/checkout',
+                element: <Checkout />,
+            },
+            {
+                path: '/coffee-delivery/confirm',
+                element: <Confirm />,
+            },
+        ],
+    },
+]);
