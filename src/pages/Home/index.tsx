2bbd38409be190d44911d56d22b74140c63d8583
Matheus Gomes <47065962+matheus369k@users.noreply.github.com>
2024-07-06 20:21:46 -0300

    create basic styles the home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,41 +1,72 @@
 import { Coffee, Package, ShoppingCart, Timer } from '@phosphor-icons/react';
 import introImage from '@assets/into-Imagem.svg';
+import coffeeImage from '@assets/coffee.png';
+import { StyledHomeIntro, StyledHomeShopList } from './styles';
 
 export function Home() {
     return (
         <main>
-            <div>
+            <StyledHomeIntro>
                 <div>
                     <h1>Encontre o café perfeito para qualquer hora do dia</h1>
                     <span>Com o Coffee Delivery você recebe seu café onde estiver, a qualquer hora</span>
                     <ul>
                         <li>
-                            <ShoppingCart size={22} weight="fill" />
+                            <i id="cart">
+                                <ShoppingCart size={16} weight="fill" />
+                            </i>
                             <p>Compra simples e segura</p>
                         </li>
                         <li>
-                            <Timer size={32} weight="fill" />
-                            <p>Entrega rápida e rastreada</p>
-                        </li>
-                        <li>
-                            <Package size={32} weight="fill" />
+                            <i id="box">
+                                <Package size={16} weight="fill" />
+                            </i>
                             <p>Embalagem mantém o café intacto</p>
                         </li>
                         <li>
-                            <Coffee size={32} weight="fill" />
+                            <i id="timer">
+                                <Timer size={16} weight="fill" />
+                            </i>
+                            <p>Entrega rápida e rastreada</p>
+                        </li>
+                        <li>
+                            <i id="coffee">
+                                <Coffee size={16} weight="fill" />
+                            </i>
                             <p>O café chega fresquinho até você</p>
                         </li>
                     </ul>
                 </div>
                 <img src={introImage} />
-            </div>
-            <div>
-                <h2>Nossos cafés</h2>
+            </StyledHomeIntro>
+            <StyledHomeShopList>
+                <div>
+                    <h2>Nossos cafés</h2>
+                    <nav>
+                        <ul>
+                            <li>
+                                <button type="button">Tradicional</button>
+                            </li>
+                            <li>
+                                <button type="button">especial</button>
+                            </li>
+                            <li>
+                                <button type="button">com leite</button>
+                            </li>
+                            <li>
+                                <button type="button">alcoólico</button>
+                            </li>
+                            <li>
+                                <button type="button">gelado</button>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
                 <ul>
                     {Array.from({ length: 14 }).map((_, index) => {
                         return (
                             <li key={index}>
-                                <img />
+                                <img src={coffeeImage} alt="" />
                                 <span>Tradicional</span>
                                 <h3>Expresso Tradicional</h3>
                                 <p>O tradicional café feito com água quente e grãos moídos</p>
@@ -52,7 +83,7 @@ export function Home() {
                         );
                     })}
                 </ul>
-            </div>
+            </StyledHomeShopList>
         </main>
     );
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,1 +1,267 @@
 import { styled } from 'styled-components';
+
+export const StyledHomeIntro = styled.div`
+    padding: 5.875rem 0;
+    min-height: calc(100vh - 6.5rem);
+
+    display: flex;
+    align-items: center;
+
+    gap: 3.5rem;
+
+    h1 {
+        margin-bottom: 1rem;
+
+        font-size: 3rem;
+        line-height: 1.3;
+        font-family: ${(props) => props.theme['title-font']}, sans-serif;
+
+        color: ${(props) => props.theme['base-title']};
+    }
+
+    span {
+        display: block;
+        margin-bottom: 4.125rem;
+
+        font-size: 1.5rem;
+        line-height: 1.3;
+        font-family: ${(props) => props.theme['title-font']}, sans-serif;
+
+        color: ${(props) => props.theme['base-subtitle']};
+    }
+
+    ul {
+        display: grid;
+        grid-template-columns: repeat(2, 1fr);
+
+        column-gap: 2.5rem;
+        row-gap: 1.25rem;
+
+        li {
+            display: flex;
+            align-items: center;
+
+            gap: 0.75rem;
+
+            width: max-content;
+
+            i {
+                height: 2rem;
+                width: 2rem;
+
+                border-radius: 50%;
+
+                display: flex;
+                align-items: center;
+                justify-content: center;
+
+                &#cart {
+                    background: ${(props) => props.theme['yellow-dark']};
+                }
+                &#timer {
+                    background: ${(props) => props.theme.yellow};
+                }
+                &#box {
+                    background: ${(props) => props.theme['base-text']};
+                }
+                &#coffee {
+                    background: ${(props) => props.theme.purple};
+                }
+
+                svg {
+                    fill: white;
+                }
+            }
+
+            span {
+                font-size: 1rem;
+                font-family: ${(props) => props.theme['text-font']}, sans-serif;
+                line-height: 1.3;
+
+                color: ${(props) => props.theme['base-text']};
+            }
+        }
+    }
+
+    img {
+        width: 29.75rem;
+        height: 22.5rem;
+    }
+`;
+
+export const StyledHomeShopList = styled.div`
+    padding: 2rem 0;
+
+    display: flex;
+    flex-direction: column;
+
+    gap: 3.5rem;
+
+    div {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+
+        h2 {
+            font-size: 2rem;
+            line-height: 1.3rem;
+
+            color: ${(props) => props.theme['base-title']};
+        }
+
+        & > nav > ul {
+            display: flex;
+            gap: 0.5rem;
+
+            li {
+                border: 1px solid ${(props) => props.theme.yellow};
+                border-radius: 100px;
+
+                button {
+                    padding: 0.375rem 0.75rem;
+
+                    background: transparent;
+                    color: ${(props) => props.theme['yellow-dark']};
+
+                    text-transform: uppercase;
+                    font-size: 0.625rem;
+                    line-height: 1.3;
+                    font-family: ${(props) => props.theme['text-font']}, sans-serif;
+                }
+            }
+        }
+    }
+
+    & > ul {
+        display: flex;
+        flex-wrap: wrap;
+
+        gap: 3rem 2rem;
+
+        li {
+            width: 16rem;
+            height: 19.375rem;
+
+            padding: 0 1.5rem 1.25rem;
+
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+
+            background: ${(props) => props.theme['base-card']};
+
+            img {
+                width: 7.5rem;
+                height: 7.5rem;
+
+                margin-bottom: 0.75rem;
+
+                margin-top: -1.5rem;
+            }
+
+            & > span {
+                margin-bottom: 1rem;
+                padding: 0.25rem 0.5rem;
+                border-radius: 100px;
+
+                color: ${(props) => props.theme['yellow-dark']};
+                background: ${(props) => props.theme['yellow-light']};
+
+                line-height: 1.3;
+                text-transform: uppercase;
+
+                font-size: 0.625rem;
+                font-family: ${(props) => props.theme['text-font']}, sans-serif;
+            }
+
+            h3 {
+                margin-bottom: 0.5rem;
+
+                line-height: 1.3;
+
+                font-size: 1.25rem;
+                font-family: ${(props) => props.theme['title-font']}, sans-serif;
+            }
+
+            p {
+                margin-bottom: 2rem;
+
+                text-align: center;
+                line-height: 1.3rem;
+
+                font-size: 0.875rem;
+                font-family: ${(props) => props.theme['text-font']}, sans-serif;
+
+                color: ${(props) => props.theme['base-label']};
+            }
+
+            div {
+                display: flex;
+                gap: 1.5rem;
+
+                span {
+                    font-family: ${(props) => props.theme['title-font']}, sans-serif;
+                    font-size: 1.5rem;
+                    font-weight: bold;
+
+                    line-height: 1.3;
+
+                    color: ${(props) => props.theme['base-text']};
+
+                    &::before {
+                        content: 'R$';
+
+                        font-family: ${(props) => props.theme['text-font']}, sans-serif;
+                        font-size: 0.875rem;
+                        font-weight: 400;
+
+                        line-height: 1.3;
+                    }
+                }
+
+                form {
+                    display: flex;
+                    gap: 0.5rem;
+
+                    input {
+                        width: 4.5rem;
+
+                        text-align: center;
+                        line-height: 1.3;
+
+                        font-size: 1rem;
+                        font-family: ${(props) => props.theme['text-font']}, sans-serif;
+
+                        background: ${(props) => props.theme['base-button']};
+                        color: ${(props) => props.theme['base-title']};
+
+                        border-radius: 6px;
+
+                        &::-webkit-outer-spin-button,
+                        &::-webkit-inner-spin-button {
+                            -webkit-appearance: none;
+                            margin: 0;
+                        }
+                    }
+
+                    button {
+                        height: 2.375rem;
+                        width: 2.375rem;
+
+                        border-radius: 6px;
+
+                        background: ${(props) => props.theme['purple-dark']};
+
+                        display: flex;
+                        justify-content: center;
+                        align-items: center;
+
+                        svg {
+                            fill: ${(props) => props.theme['base-card']};
+                        }
+                    }
+                }
+            }
+        }
+    }
+`;
