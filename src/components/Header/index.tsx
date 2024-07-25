916a6aa8126aca00edd12a7c81961d589d384e32
Matheus Gomes <47065962+matheus369k@users.noreply.github.com>
2024-07-25 20:34:18 -0300

    add redirect to page checkout

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,9 +3,15 @@ import logoCoffeeDelivery from '@assets/logo.svg';
 import { StyledHeader } from './styles';
 import { useContext } from 'react';
 import { CountProductsContext } from '@/contexts/context-count-products';
+import { useNavigate } from 'react-router-dom';
 
 export function Header() {
+    const navigate = useNavigate();
     const { countProducts } = useContext(CountProductsContext);
+
+    function handleRedirectToCheckoutPage() {
+        navigate('/coffee-delivery/checkout');
+    }
 
     return (
         <StyledHeader>
@@ -16,7 +22,7 @@ export function Header() {
                     <MapPin size={22} weight="fill" />
                     <span>São Paulo, SP</span>
                 </p>
-                <button data-count-products={countProducts ? countProducts.length : 0} type="button">
+                <button onClick={handleRedirectToCheckoutPage} data-count-products={countProducts ? countProducts.length : 0} type="button">
                     <ShoppingCart size={22} weight="fill" />
                 </button>
             </div>
