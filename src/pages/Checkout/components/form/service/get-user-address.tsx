6c6881a62fdaeb2b0549179c912bc91e15162d6c
Matheus Gomes <47065962+matheus369k@users.noreply.github.com>
2024-09-07 16:36:15 -0300

    uncouple resquest to api from index file Checkout/component/form

---
+++ src/pages/Checkout/components/form/service/get-user-address.tsx
@@ -0,0 +1,33 @@
+import { AddressType } from '..';
+import { api } from '../../../../../lib/api';
+
+export interface GetUserAddressPropsType {
+    addressId: string;
+    handleDisabledEditeAddress: () => void;
+    setInUseFormNewAddress: (addressResponse: AddressType) => void;
+    setAddress: React.Dispatch<React.SetStateAction<AddressType>>;
+}
+
+export function GetUserAddress({
+    addressId,
+    setAddress,
+    setInUseFormNewAddress,
+    handleDisabledEditeAddress,
+}: GetUserAddressPropsType) {
+    api.get(`/user/${addressId}`)
+        .then((response) => {
+            setAddress((state) => {
+                const addressResponse: AddressType = {
+                    ...state,
+                    ...response.data.address,
+                };
+
+                setInUseFormNewAddress(addressResponse);
+
+                return addressResponse;
+            });
+
+            handleDisabledEditeAddress();
+        })
+        .catch((error: Error) => console.log(error.message));
+}
