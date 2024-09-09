eeff368216cb78549547c473500c4814a1489ddc
Matheus Gomes <47065962+matheus369k@users.noreply.github.com>
2024-09-09 17:18:40 -0300

    resolve error: alias paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,22 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-import * as path from 'path';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    base: '/coffee-delivery',
     resolve: {
         alias: {
-            '@pages': path.resolve(__dirname, 'src/pages'),
-            '@components': path.resolve(__dirname, 'src/components'),
-            '@assets': path.resolve(__dirname, 'src/assets'),
-            '@styles': path.resolve(__dirname, 'src/styles'),
-            '@': path.resolve(__dirname, 'src'),
+            '@pages': path.resolve(__dirname, './src/pages'),
+            '@components': path.resolve(__dirname, './src/components'),
+            '@assets': path.resolve(__dirname, './src/assets'),
+            '@styles': path.resolve(__dirname, './src/styles'),
+            '@lib': path.resolve(__dirname, './src/lib'),
+            '@layout': path.resolve(__dirname, './src/layout'),
+            '@contexts': path.resolve(__dirname, './src/contexts'),
+            '@router': path.resolve(__dirname, './src/router'),
+            '@': path.resolve(__dirname, './src'),
         },
     },
-    base: '/coffee-delivery',
 });
