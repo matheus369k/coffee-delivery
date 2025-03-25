0a5e71a6dadcba22f5d2a5ae0d7a1ec14213c443
matheus369k <gomes.craft10@gmail.com>
2025-03-25 19:52:02 -0300

    chore: update vite.config at alias

--- vite.config.ts
+++ vite.config.ts
@@ -4,19 +4,19 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    base: '/coffee-delivery',
-    resolve: {
-        alias: {
-            '@pages': path.resolve(__dirname, './src/pages'),
-            '@components': path.resolve(__dirname, './src/components'),
-            '@assets': path.resolve(__dirname, './src/assets'),
-            '@styles': path.resolve(__dirname, './src/styles'),
-            '@lib': path.resolve(__dirname, './src/lib'),
-            '@layout': path.resolve(__dirname, './src/layout'),
-            '@contexts': path.resolve(__dirname, './src/contexts'),
-            '@router': path.resolve(__dirname, './src/router'),
-            '@': path.resolve(__dirname, './src'),
-        },
+  plugins: [react()],
+  base: '/coffee-delivery',
+  resolve: {
+    alias: {
+      '@pages': path.resolve(__dirname, 'src/pages'),
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@assets': path.resolve(__dirname, 'src/assets'),
+      '@styles': path.resolve(__dirname, 'src/styles'),
+      '@lib': path.resolve(__dirname, 'src/lib'),
+      '@layout': path.resolve(__dirname, 'src/layout'),
+      '@contexts': path.resolve(__dirname, 'src/contexts'),
+      '@router': path.resolve(__dirname, 'src/router'),
+      '@': path.resolve(__dirname, 'src'),
     },
+  },
 });
